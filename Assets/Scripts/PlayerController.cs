15f97ee5946fc544cb00659c3898e05441de54da
Ralph Roberts <ralphrobertsuk@gmail.com>
2021-02-01 22:01:04 +0000

    add movement for player and ability to change light

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public Vector3 playerSpeed;
+    private PlayerInputs inputController;
+    private PlayerInputs.PlayerActions movementInputMap;
+    public float moveSpeed;
+    Vector2 movement;
+    Vector3 cameraForward;
+    Vector3 cameraRight;
+    Vector3 XZPlaneNormal = new Vector3(0, 1, 0);
+    public Camera cam;
+    public Light lantern;
+    Color lightColour;
+    Color[] colours = { new Color(1, 0, 0), new Color(0, 1, 0), new Color(0, 0, 1) };
+    int colourIndex = 0;
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        inputController = new PlayerInputs();
+
+        movementInputMap = inputController.Player;
+
+        movementInputMap.Movement.performed += ctx => OnMovement(ctx);
+        movementInputMap.Movement.started += ctx => OnMovement(ctx);
+        movementInputMap.Movement.canceled += ctx => OnMovement(ctx);
+
+        movementInputMap.Light.started += _ => ChangeLight();
+
+    }
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        cameraForward = Vector3.ProjectOnPlane(cam.transform.forward, XZPlaneNormal);
+        cameraRight = Vector3.ProjectOnPlane(cam.transform.right, XZPlaneNormal);
+        lantern.color = colours[colourIndex];
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(playerSpeed);
+        
+        rb.velocity = cameraForward * movement.y * moveSpeed + cameraRight * movement.x * moveSpeed;
+    }
+
+    void ChangeLight()
+    {
+        colourIndex = (colourIndex + 1) % 3;
+        lantern.color = colours[colourIndex];
+    }
+
+    public void OnMovement(InputAction.CallbackContext ctx)
+    {
+        Vector2 newMovementInput = ctx.ReadValue<Vector2>();
+        movement = newMovementInput;
+        
+        Debug.Log(newMovementInput);
+    }
+
+    private void OnEnable()
+    {
+        inputController.Enable();
+    }
+    private void OnDisable()
+    {
+        inputController.Disable();
     }
 }
