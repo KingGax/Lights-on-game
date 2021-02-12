e2a4e82fefc02a5f4c6aac577a65c07da8d1b8b5
Ralph Roberts <ralphrobertsuk@gmail.com>
2021-02-12 19:19:24 +0000

    enemies cannot be shot while lit

--- Assets/Scripts/LightableEnemy.cs
+++ Assets/Scripts/LightableEnemy.cs
@@ -7,8 +7,12 @@ public class LightableEnemy : LightableObject
 {
     //public NavMeshObstacle obstacle;
     IEnemy enemy;
+    int defaultEnemyLayer;
+    int hiddenEnemyLayer;
     void Awake(){
         enemy = gameObject.GetComponentInParent<IEnemy>();
+        defaultEnemyLayer = transform.parent.gameObject.layer;
+        hiddenEnemyLayer = LayerMask.NameToLayer("HiddenEnemies");
     }
     override public void Appear()
     {
@@ -16,6 +20,7 @@ public class LightableEnemy : LightableObject
         //obstacle.enabled = true;
         //base.Appear();
         enemy.EnableAI();
+        transform.parent.gameObject.layer = defaultEnemyLayer;
     }
     override public void Disappear()
     {
@@ -23,6 +28,7 @@ public class LightableEnemy : LightableObject
         //obstacle.enabled = false;
         //base.Disappear();
         enemy.DisableAI();
+        transform.parent.gameObject.layer = hiddenEnemyLayer;
     }
 
     public override bool CheckNoIntersections()
