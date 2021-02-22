9434cd58c059a24a8dd406ead4b48a79895c44ac
vic-kush <45151311+vic-kush@users.noreply.github.com>
2021-02-23 01:07:30 +0200

    basic butchered lobby

---
+++ Assets/Scripts/Manus/RoomListingInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class RoomListingInfo : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Text text;
+
+    private RoomInfo _roomInfo;
+
+    public void SetRoomInfo(RoomInfo roomInfo)
+    {
+        _roomInfo = roomInfo;
+        text.text = roomInfo.Name;
+    }
+
+    public void OnCLick_Button()
+    {
+        PhotonNetwork.JoinRoom(_roomInfo.Name);
+    }
+}
---
+++ Assets/Scripts/Manus/RoomListingsMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    private Transform _content;
+
+
+    [SerializeField]
+    public GameObject _roomListing;
+
+    private Dictionary<string, GameObject> cachedRoomList = new Dictionary<string, GameObject>();
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        for(int i=0; i<roomList.Count; i++)
+        {
+            RoomInfo info = roomList[i];
+            if (info.RemovedFromList)
+            {
+                if (cachedRoomList.ContainsKey(info.Name)){
+                    Destroy(cachedRoomList[info.Name]);
+                    cachedRoomList.Remove(info.Name);
+                }
+            }
+            else
+            {
+                GameObject listing =  Instantiate(_roomListing,_content);
+                RoomListingInfo roomInfo = listing.GetComponent<RoomListingInfo>();
+                roomInfo.SetRoomInfo(info);
+                cachedRoomList[info.Name] = listing;
+            }
+        }
+    }
+}
