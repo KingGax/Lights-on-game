9526c79cf91652a16e37d8d830d10ffd328cb68e
Rob <rob.russmann@googlemail.com>
2021-02-24 14:52:46 +0000

    Fixed gamemanager spawning players in room scene

--- Assets/Scripts/Netcode/GameManager.cs
+++ Assets/Scripts/Netcode/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviourPunCallbacks {
             Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
         }
         Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-        PhotonNetwork.LoadLevel("Room");
+        PhotonNetwork.LoadLevel("Spawner");
     }
 
     public override void OnPlayerEnteredRoom(Player other) {
