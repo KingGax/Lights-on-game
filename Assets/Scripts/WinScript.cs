b930388b8a8be1dc52fb43d8d7fd88688afd6ea6
danjones1618 <hello@danjones.dev>
2021-02-28 13:09:55 +0000

    Add End Scene

--- Assets/Scripts/WinScript.cs
+++ Assets/Scripts/WinScript.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class WinScript : MonoBehaviour
-{
+public class WinScript : MonoBehaviour {
     int playerLayer;
     private void Start() {
         playerLayer = LayerMask.NameToLayer("Player");
     }
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == playerLayer) {
-            //TODO implement win popup
-            Debug.Log("Win popup");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
