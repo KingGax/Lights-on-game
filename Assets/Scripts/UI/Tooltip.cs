c2342f97de697be849bfa9f0942026a2e505a4a3
danjones1618 <hello@danjones.dev>
2021-02-28 19:50:08 +0000

    Add tooltip prefab

--- Assets/Scripts/UI/Tooltip.cs
+++ Assets/Scripts/UI/Tooltip.cs
@@ -1,12 +1,62 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
+public enum TooltipOrientation {
+    X, Z, Camera
+}
+
+[System.Serializable]
 public class Tooltip : MonoBehaviour {
 
-    public Transform target;
+    [SerializeField]
+    private string text;
+    [SerializeField]
+    private TooltipOrientation orientation;
+    private Transform target;
+    private float t;
+
+    public void Awake() {
+        target = transform;
+        Text = text;
+        Orientation = orientation;
+    }
 
     public void Update() {
+        t += Time.deltaTime;
+        transform.position = target.position
+            + new Vector3(0.0f, 0.01f * Mathf.Sin(t), 0.0f);
+    }
 
-        transform.position = Camera.main.WorldToScreenPoint(target.position);
+    public string Text {
+        get {return text;}
+        set {
+            text = value;
+            TextMeshProUGUI label = GetComponentInChildren<TextMeshProUGUI>();
+            label.text = text;
+
+            RectTransform canvas = GetComponentInChildren<RectTransform>();
+            canvas.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, (16 * text.Length) + 64);
+        }
+    }
+
+
+    public TooltipOrientation Orientation{
+        get {return orientation;}
+        set {
+            orientation = value;
+            switch (value) {
+                case TooltipOrientation.X:
+                    transform.right = Vector3.forward;
+                    break;
+                case TooltipOrientation.Z:
+                    transform.right = Vector3.left;
+                    break;
+                case TooltipOrientation.Camera:
+                default:
+                    transform.rotation = Camera.main.transform.rotation;
+                    break;
+            }
+        }
     }
 }
