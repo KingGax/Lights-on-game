9418e3019f8f0c96cc5345bb1f6665151eae0144
Ralph Roberts <ralphrobertsuk@gmail.com>
2021-02-28 14:36:08 +0000

    cherrpick2

--- Assets/Scripts/LightingScripts/LightableRangedEnemy.cs
+++ Assets/Scripts/LightingScripts/LightableRangedEnemy.cs
@@ -6,9 +6,11 @@ public class LightableRangedEnemy : LightableEnemy
 {
     public SkinnedMeshRenderer smr;
     public EnemyGun gunScript;
+    private Animator anim;
 
     public override void Start() {
         overrideMeshRenderer = true;
+        anim = transform.parent.GetComponent<Animator>();
         base.Start();
     }
     public override void SetColour()
@@ -21,11 +23,13 @@ public class LightableRangedEnemy : LightableEnemy
     }
 
     public override void Disappear() {
+        anim.speed = 0;
         smr.material = hiddenMaterial;
         smr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
         base.Disappear();
     }
     public override void Appear() {
+        anim.speed = 1;
         smr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
         smr.material = defaultMaterial;
         base.Appear();
