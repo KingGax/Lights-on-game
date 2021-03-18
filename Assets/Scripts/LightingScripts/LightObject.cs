45042e5db2edc8c0a6744f76542c76009a696217
Ralph Roberts <ralphrobertsuk@gmail.com>
2021-03-18 17:29:32 +0000

    shrunk player range, fix bullet bug and removed negative ammo

--- Assets/Scripts/LightingScripts/LightObject.cs
+++ Assets/Scripts/LightingScripts/LightObject.cs
@@ -24,16 +24,16 @@ public class LightObject : MonoBehaviour {
         colour = playerLantern.color;
         sphere = GetComponent<SphereCollider>();
         lightRange = playerLantern.range;
-        range = lightRange / 1.3f;
+        range = lightRange / 1.8f;
         sphere.radius = range;
         lightLayer = 1 << LayerMask.NameToLayer("LightingHitboxes");
     }
 
     public void Update() {
-        playerLantern = GetComponent<Light>();
+        /*playerLantern = GetComponent<Light>();
         sphere = GetComponent<SphereCollider>();
         lightRange = playerLantern.range;
-        sphere.radius = lightRange / 1.3f;
+        sphere.radius = lightRange / 1.3f;*/
     }
 
     public float GetRange() {
