e6973010455f0d5b2105c58a4ae07ab5cee8171c
vic-kush <45151311+vic-kush@users.noreply.github.com>
2021-03-21 18:37:28 +0200

    puzzle synced ?

---
+++ Assets/Scripts/Puzzles/PuzzleRoom.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PuzzleRoom : RoomObjective
+{
+
+    bool started = false;
+
+    bool objectiveComplete = false;
+
+    public PuzzleDoorTrigger trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void StartObjective() {
+        started = true;
+        LockEntrancesGlobal();
+        LockExitGlobal();
+        // StartNewSetWave();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (pv == null || !pv.IsMine) return;
+        if(!objectiveComplete && trigger.unlocked){
+                UnlockExitGlobal();
+                objectiveComplete = true;
+        }
+    }
+}
