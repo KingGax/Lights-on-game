52421d7405fdeb5892374102fadf2517e23fc40e
danjones1618 <hello@danjones.dev>
2021-04-04 23:06:53 +0100

    Some changes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
@@ -7,16 +5,14 @@ public class CameraController : MonoBehaviour {
     public Vector3 offset;
     public Vector3 rotation;
 
-    public void bindToPlayer(Transform viewOwner)
-    {
+    public void bindToPlayer(Transform viewOwner) {
         this.player = viewOwner;
     }
-    // Start is called before the first frame update
+
     void Start() {
         transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
     }
 
-    // Update is called once per frame
     void Update() {
         transform.position = player.transform.position + offset;
     }
--- Assets/Scripts/Projectiles/EnemyBullets.cs
+++ Assets/Scripts/Projectiles/EnemyBullets.cs
@@ -10,6 +10,7 @@ public class EnemyBullets : BulletController {
         base.Awake();
         walls = GlobalValues.Instance.environment;
     }
+
     protected override void OnTriggerEnter(Collider other) {
         if (((1 << other.gameObject.layer) & walls) != 0) {
             if (pv == null || !pv.IsMine) return;
