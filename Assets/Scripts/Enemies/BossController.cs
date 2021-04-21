43ed2e2b49516a920adab3b3cb85135b7b69cb0b
Rob <rob.russmann@googlemail.com>
2021-04-21 22:05:34 +0100

    Slightly more boilerplate stuff

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -40,16 +40,163 @@ public class BossController : Enemy
         Reappearing, //Reapearring and performing AoE attack
         Staggered //Staggered/stunned (after taking a certain amount of damage) [not sure if I want to use this]
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         currentPhase = 0; 
         enemyState = EnemyState.Patrolling;
+        StartCoroutine("EnemyTimers");
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pv == null || !pv.IsMine) return;
+        // if (!hasPlayerJoined){
+        //     if (GlobalValues.Instance != null && GlobalValues.Instance.players.Count > 0){
+        //         hasPlayerJoined = true;
+        //         int index = SelectTarget();
+        //         weapon.SetTarget(index);
+        //     } else {
+        //         return;
+        //     }
+        // } 
+        ManageStates();
+    }
+
+    void ManageStates(){
+        if (aiEnabled) {
+            switch (enemyState) {
+                case EnemyState.DecisionState:
+                    MakeDecision();
+                    break;
+                case EnemyState.RotateShooting:
+                    RotateShoot();
+                    break;
+                case EnemyState.MissileAttack:
+                    MissileAttack();
+                    break;
+                case EnemyState.Patrolling:
+                    Patrol();
+                    break;
+                case EnemyState.AOEMeleeStartup:
+                    AOEMeleeStartup();
+                    break;
+                case EnemyState.AOEMelee:
+                    AOEMelee();
+                    break;
+                case EnemyState.AOEMeleeRecovery:
+                    AOEMeleeRecovery();
+                    break;
+                case EnemyState.SummonAdds:
+                    SummonAdds();
+                    break;
+                case EnemyState.SwarmRepositioning:
+                    SwarmReposition();
+                    break;
+                case EnemyState.Reappearing:
+                    ReappearState();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void MakeDecision(){
+
+    }
+
+    void ChangeToRotateShoot(){
+        enemyState = EnemyState.RotateShooting;
+    }
+
+    void RotateShoot(){
+
+    }
+
+    void ChangeToMissileAttack(){
+        enemyState = EnemyState.MissileAttack;
+        missileShotsFired = 0;
+    }
+
+    void MissileAttack(){
+
+    }
+
+    void Patrol(){
+
+    }
+
+    void ChangeToAOEMeleeStartup(){
+        enemyState = EnemyState.AOEMeleeStartup;
+        aoeStartTimer = aoeStartTimerMax;
+    }
+
+    void AOEMeleeStartup(){
+
+    }
+
+    void ChangeToAOEMelee(){
+        enemyState = EnemyState.AOEMelee;
+    }
+
+    void AOEMelee(){
+
+    }
+    void ChangeToAOEMeleeRecovery(){
+        enemyState = EnemyState.AOEMeleeRecovery;
+        aoeEndTimer = aoeEndTimerMax;
+    }
+
+    void AOEMeleeRecovery(){
+
+    }
+
+    void ChangeToSummonAdds(){
+        enemyState = EnemyState.SummonAdds;
+    }
+
+    void SummonAdds(){
+
+    }
+
+    void ChangeToSwarmReposition(){
+        enemyState = EnemyState.SwarmRepositioning;
+    }
+    
+    void SwarmReposition(){
+
+    }
+
+    void ChangeToReappearState(){
+        enemyState=EnemyState.Reappearing;
+    }
+
+    void ReappearState(){
+
+    }
+
+    // Start is called before the first frame update
+    
+
+    private IEnumerator EnemyTimers() {
+        while (true) {
+            if (aoeStartTimer > 0) {
+                aoeStartTimer -= Time.deltaTime;
+            }
+
+            if (aoeEndTimer > 0) {
+                aoeEndTimer -= Time.deltaTime;
+            }
+            if (rotatingShotTimer > 0) {
+                rotatingShotTimer -= Time.deltaTime;
+            }
+            if (reappearingTimer > 0) {
+                reappearingTimer -= Time.deltaTime;
+            }
+            yield return null;
+        }
     }
 }
