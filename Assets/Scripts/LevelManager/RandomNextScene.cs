1d2967c62a2d83639b1da06cb3052d3cc705f266
vic-kush <45151311+vic-kush@users.noreply.github.com>
2021-05-12 16:53:19 +0300

    random scene sequence

---
+++ Assets/Scripts/LevelManager/RandomNextScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNextScene : MonoBehaviour
+{
+    public WinScript winScript;
+
+    public List<string> scenes;
+
+    // Start is called before the first frame update
+    void Start(){
+        generateRandomNextScene();
+    }
+
+    int getSceneIndex(int rand){
+        if(rand < 4) return 0;
+        else if(rand < 7) return 1;
+        else return 2;
+    }
+
+    void generateRandomNextScene(){
+        int sceneNum = Random.Range(0,10);
+        Debug.Log("Scene Random Num: "+ sceneNum);
+        winScript.sceneName = scenes[getSceneIndex(sceneNum)];
+    }
+}
