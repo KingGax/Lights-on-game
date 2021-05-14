fbb12d726d45065290ac48c4caf3bb6840281a20
Bogdan Dumitrescu <bogdoomy@gmail.com>
2021-05-14 02:32:17 +0100

    Make audio work
    Co-authored-by: danjones1618<hello@danjones.dev>

--- Assets/Scripts/Menus/VoiceMenu.cs
+++ Assets/Scripts/Menus/VoiceMenu.cs
@@ -5,19 +5,24 @@ using LightsOn.AudioSystem;
 
 public class VoiceMenu : MonoBehaviour {
 
+    public Animator transition;
+
     [DllImport("__Internal")]
     private static extern void setupMicrophoneUnity();
 
     public void OnRejectClicked() {
-        SceneManager.LoadScene("StartMenu");
+        AudioManager.Instance.PlayNext();
+        transition.SetTrigger("Start");
+        Invoke("ChangeScene", 1);
     }
 
     public void OnAcceptClicked() {
         setupMicrophoneUnity();
         //Debug.Log("Need to enable voice chat here");
+        //AudioManager.Instance.PlayNext();
     }
 
-    public void Awake() {
-        AudioManager.Instance.PlayNext();
+    private void ChangeScene() {
+        SceneManager.LoadScene("StartMenu");
     }
 }
