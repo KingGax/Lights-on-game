a6095f1e189c313e59cfd2bcbf21b9f13000e452
danjones1618 <hello@danjones.dev>
2021-05-17 03:08:17 +0100

    Fix audio

--- Assets/Scripts/Audio/Clip.cs
+++ Assets/Scripts/Audio/Clip.cs
@@ -9,20 +9,42 @@ namespace LightsOn.AudioSystem {
         public float start;
         public float end;
         public ClipEndBehaviour endBehaviour;
+        public float[] shortCicuiteLocations;
 
         public float length() {
             return end - start;
         }
 
-        public float getNextBeatAfterTime(float t) {
-            float tbeats = Mathf.Ceil(t * bpm / 60.0f);
-            return tbeats * 60.0f/bpm;
+        public float getNextBeatAfterTime(float startT, float t) {
+            switch (endBehaviour) {
+                case ClipEndBehaviour.LOOP:
+                case ClipEndBehaviour.NEXT:
+                    // No short circuiting
+                    Debug.Log("LOOP/NEXT end calced: " + startT + " " + t
+                        + " " + start + " " + end + " " + (startT + end)
+                    );
+                    return startT + end;
+                case ClipEndBehaviour.LOOPSHORT:
+                    foreach (float l in shortCicuiteLocations) {
+                        if (t < startT + l) {
+                            return startT + l;
+                        }
+                    }
+                    return startT + end;
+                case ClipEndBehaviour.END:
+                case ClipEndBehaviour.LOOPBEAT:
+                default:
+                    float tbeats = Mathf.Ceil(t * bpm / 60.0f);
+                    return tbeats * 60.0f/bpm;
+            }
         }
     }
 
     public enum ClipEndBehaviour {
         END,
         LOOP,
-        NEXT
+        LOOPSHORT,
+        NEXT,
+        LOOPBEAT
     }
 }
