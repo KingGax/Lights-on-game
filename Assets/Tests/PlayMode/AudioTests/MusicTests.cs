7288ac0a27555ecdb419d1cc5a535e7587729283
danjones1618 <hello@danjones.dev>
2021-05-20 21:57:02 +0100

    Updates

--- Assets/Tests/PlayMode/AudioTests/MusicTests.cs
+++ Assets/Tests/PlayMode/AudioTests/MusicTests.cs
@@ -22,6 +22,7 @@ namespace AudioTests {
             listner = new GameObject();
             listner.AddComponent<AudioListener>();
             yield return new EnterPlayMode();
+            yield return new WaitForSeconds(0.5f);
         }
 
         [UnityTearDown]
@@ -45,50 +46,23 @@ namespace AudioTests {
         }
 
         [UnityTest]
-        public IEnumerator TestPlayNextSwapsSrc() {
-            AudioSource[] srcs = audioMgr.GetComponentsInChildren<AudioSource>();
-            int idx = 0;
+        public IEnumerator PlayNextSwapsSrc() {
+            int idx = AudioManager.Instance.freeAudioSource;
+            AudioManager.Instance.PlayNext();
+            yield return null;
+            Assert.AreNotEqual(AudioManager.Instance.freeAudioSource, idx);
+        }
 
+        [UnityTest]
+        public IEnumerator PlayNextChangesClip() {
             yield return new WaitForSeconds(0.5f);
-            for (int i = 0; i < srcs.Length; i++) {
-                if (srcs[i].isPlaying) {
-                    idx = i;
-                }
-            }
             AudioManager.Instance.PlayNext();
             yield return new WaitForSeconds(1.5f);
-
-            for (int i = 0; i < srcs.Length; i++) {
-                if (srcs[i].isPlaying) {
-                    Assert.IsTrue(idx != i);
-                }
-            }
+            Assert.AreEqual(1, AudioManager.Instance.playingTrack);
         }
 
         [UnityTest]
-        public IEnumerator TestPlayNextChangesClip() {
-            // WARNING: only works because first and second clip on prefab are different
-            AudioSource[] srcs = audioMgr.GetComponentsInChildren<AudioSource>();
-            AudioClip idx = null;
-
-            yield return new WaitForSeconds(0.5f);
-            for (int i = 0; i < srcs.Length; i++) {
-                if (srcs[i].isPlaying) {
-                    idx = srcs[i].clip;
-                }
-            }
-            AudioManager.Instance.PlayNext();
-            yield return new WaitForSeconds(2f);
-
-            for (int i = 0; i < srcs.Length; i++) {
-                if (srcs[i].isPlaying) {
-                    Assert.IsTrue(idx != srcs[i].clip);
-                }
-            }
-        }
-
-        [UnityTest]
-        public IEnumerator TestPlayNextOnlyOnePlaying() {
+        public IEnumerator PlayNextOnlyOnePlaying() {
             AudioSource[] srcs = audioMgr.GetComponentsInChildren<AudioSource>();
             int idx = 0;
 
@@ -106,7 +80,7 @@ namespace AudioTests {
         }
 
         [UnityTest]
-        public IEnumerator TestLoops() {
+        public IEnumerator Loops() {
             AudioSource[] srcs = audioMgr.GetComponentsInChildren<AudioSource>();
             int idx = 0;
 
@@ -126,7 +100,7 @@ namespace AudioTests {
         }
 
         [UnityTest]
-        public IEnumerator TestLoopOnlyOnePlaying() {
+        public IEnumerator LoopOnlyOnePlaying() {
             AudioSource[] srcs = audioMgr.GetComponentsInChildren<AudioSource>();
             int idx = 0;
 
